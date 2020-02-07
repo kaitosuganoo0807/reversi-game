d99d1f05a9dc67acb5d790c48f951dc114f93711
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-02-07 20:52:02 +0900

    update

--- js/game.core.js
+++ js/game.core.js
@@ -1,3 +1,10 @@
 "use strict";
 
 // ライブラリ用のオブジェクトの作成
+if (window.game === undefined) {window.game = {}}
+if (window.game.core === undefined) {window.game.core = {}}
+
+(function() {
+  var _t = game.core; //ショートカットキーの作成
+  
+})
